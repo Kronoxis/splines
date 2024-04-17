5567bb3ca43c147de2e7da1da24f26e5ac5ab259
Kronoxis <dev.kronoxis@gmail.com>
2024-04-17 18:11:11 +0200

    Add simple test scene

---
+++ Assets/Dreamteck/Test/SplineProjectorLoopTriggerInvocation/LogTrigger.cs
@@ -0,0 +1,20 @@
+using Dreamteck.Splines;
+using UnityEngine;
+
+public class LogTrigger : MonoBehaviour
+{
+    public void Log1(SplineUser user)
+    {
+        Debug.Log("Trigger 1");
+    }
+
+    public void Log2(SplineUser user)
+    {
+        Debug.Log("Trigger 2");
+    }
+
+    public void Log3(SplineUser user)
+    {
+        Debug.Log("Trigger 3");
+    }
+}
